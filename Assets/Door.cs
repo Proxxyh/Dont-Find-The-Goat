38139d0f90eeb9e3aecefbb426cbad719bd4e046
Proxxyh <erenbbo@hotmail.com>
2024-06-22 22:25:00 +0300

    Base

---
+++ Assets/Door.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] public int doorIndex;
+    [SerializeField] public bool isOpen;
+
+}
