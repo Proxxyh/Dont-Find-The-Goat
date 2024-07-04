b0807443706d1c8247c6bd73615d30ba5268c9df
Proxxyh <erenbbo@hotmail.com>
2024-07-05 00:28:56 +0300

    Stats Menu Button Added
    Stats Menu Button Added
    Help button updated

--- Assets/Scripts/UI Scripts/MainMenuPanel.cs
+++ Assets/Scripts/UI Scripts/MainMenuPanel.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MainMenuPanel : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> mainMenuObjects;
+    [SerializeField] private List<GameObject> helpObjects;
+
     public void PlayButton()
     {
         StateManager.Instance.ChangeCurrentState(GameStates.ThreeDoorPickOne);
@@ -11,11 +14,21 @@ public class MainMenuPanel : MonoBehaviour
 
     public void HelpButton()
     {
-        transform.Find("HelpSection").gameObject.SetActive(true);
+        for (int i = 0; i < mainMenuObjects.Count; i++)
+        {
+            mainMenuObjects[i].SetActive(false);
+        }
+        for (int i = 0; i < helpObjects.Count; i++)
+        {
+            helpObjects[i].SetActive(true);
+        }
 
-        transform.Find("MainButtons").gameObject.SetActive(false);
-        transform.Find("DeveloperTextGameObject").gameObject.SetActive(false);
-        transform.Find("PixelArtistTextGameObject").gameObject.SetActive(false);
+
+        //transform.Find("HelpSection").gameObject.SetActive(true);
+
+        //transform.Find("MainButtons").gameObject.SetActive(false);
+        //transform.Find("DeveloperTextGameObject").gameObject.SetActive(false);
+        //transform.Find("PixelArtistTextGameObject").gameObject.SetActive(false);
     }
 
     public void ExitButton()
@@ -30,10 +43,21 @@ public class MainMenuPanel : MonoBehaviour
         StateManager.Instance.reStartButton.SetActive(false);
         StateManager.Instance.ChangeCurrentState(GameStates.None);
         StateManager.Instance.ChangeCurrentState(GameStates.MainMenu);
-        transform.Find("MainButtons").gameObject.SetActive(true);
-        transform.Find("HelpSection").gameObject.SetActive(false);
-        transform.Find("DeveloperTextGameObject").gameObject.SetActive(true);
-        transform.Find("PixelArtistTextGameObject").gameObject.SetActive(true);
+
+        for (int i = 0; i < helpObjects.Count; i++)
+        {
+            helpObjects[i].SetActive(false);
+        }
+        for (int i = 0; i < mainMenuObjects.Count; i++)
+        {
+            mainMenuObjects[i].SetActive(true);
+        }
+
+
+        //transform.Find("MainButtons").gameObject.SetActive(true);
+        //transform.Find("HelpSection").gameObject.SetActive(false);
+        //transform.Find("DeveloperTextGameObject").gameObject.SetActive(true);
+        //transform.Find("PixelArtistTextGameObject").gameObject.SetActive(true);
 
     }
 }
