d83414a3857b96d46679036f63d3472d965fc26f
Proxxyh <erenbbo@hotmail.com>
2024-07-05 05:47:41 +0300

    Animation Manager Updated
    Main menu buttons animation added

--- Assets/Scripts/UI Scripts/AnimationManager.cs
+++ Assets/Scripts/UI Scripts/AnimationManager.cs
@@ -1,15 +1,16 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-    public void MainMenuButtonEnterAnimation()
+    public void MainMenuButtonEnterAnimation(GameObject obj)
     {
-
+        obj.GetComponent<RectTransform>().DOScale(1.1f, 0.5f);
     }
-    public void MainMenuButtonExitAnimation()
+    public void MainMenuButtonExitAnimation(GameObject obj)
     {
-
+        obj.GetComponent<RectTransform>().DOScale(1f, 0.5f);
     }
 }
--- Assets/Scripts/UI Scripts/StatsUIManager.cs
+++ Assets/Scripts/UI Scripts/StatsUIManager.cs
@@ -71,11 +71,8 @@ public class StatsUIManager : MonoBehaviour
 
         resetDataTMP.text = so.resetDataText;
     }
-
-
-    public void StatsButton()
+    public void ToggleMenu()
     {
-        UpdateStatTexts();
         if (this.transform.GetChild(0).gameObject.activeSelf)
         {
             DOTween.KillAll();
@@ -124,9 +121,63 @@ public class StatsUIManager : MonoBehaviour
             tmpText.color = colorList[6];
             tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 1).SetDelay(delay).From(); //WhenPlayerNotChangeStats Anim
         }
+    }
+    public void ToggleMenu(bool showOrHide)
+    {
+        if (!showOrHide)
+        {
+            DOTween.KillAll();
+            this.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(showOrHide)
+        {
+            TMP_Text tmpText;
+            UnityEngine.UI.Image image = this.transform.GetChild(0).GetChild(6).GetComponent<UnityEngine.UI.Image>();
+
+            float delay;
 
 
+            this.transform.GetChild(0).gameObject.SetActive(true);
 
+            delay = 0.5f;
+            this.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            this.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().DOScaleY(0, delay).From(); //BlackBack Anim
+
+            tmpText = this.transform.GetChild(0).GetChild(1).GetComponent<TMP_Text>();
+            tmpText.color = colorList[0];
+            tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 0.5f).From().SetDelay(0.1f); //StatsTitle Anim
+
+            tmpText = this.transform.GetChild(0).GetChild(2).GetComponent<TMP_Text>();
+            tmpText.color = colorList[1];
+            tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 0.5f).From().SetDelay(0.2f); //WhenPlayerChangeTitle Anim
+
+            tmpText = this.transform.GetChild(0).GetChild(3).GetComponent<TMP_Text>();
+            tmpText.color = colorList[2];
+            tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 0.5f).From().SetDelay(0.2f); //WhenPlayerChangeStats Anim
+
+            tmpText = this.transform.GetChild(0).GetChild(4).GetComponent<TMP_Text>();
+            tmpText.color = colorList[3];
+            tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 0.5f).From().SetDelay(0.4f); //WhenPlayerNotChangeTitle Anim
+
+            tmpText = this.transform.GetChild(0).GetChild(5).GetComponent<TMP_Text>();
+            tmpText.color = colorList[4];
+            tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 0.5f).From().SetDelay(0.4f); //WhenPlayerNotChangeStats Anim
+
+
+            image = this.transform.GetChild(0).GetChild(6).GetComponent<UnityEngine.UI.Image>();
+            image.color = colorList[5];
+            image.DOColor(new Color(image.color.r, image.color.g, image.color.b, 0), delay + 1f).From().SetDelay(delay);
+
+            tmpText = this.transform.GetChild(0).GetChild(6).GetChild(0).GetComponent<TMP_Text>();
+            tmpText.color = colorList[6];
+            tmpText.DOColor(new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, 0), delay + 1).SetDelay(delay).From(); //WhenPlayerNotChangeStats Anim
+        }
+    }
+
+    public void StatsButton()
+    {
+        UpdateStatTexts();
+        ToggleMenu();
     }
     public void ResetDataButton()
     {
